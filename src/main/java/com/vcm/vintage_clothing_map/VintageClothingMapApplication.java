576121b77e85a17d14d6145c25d23ec5fd5b1aba
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-09-16 17:01:17 +0900

    feat: Spring Initializr

---
+++ src/main/java/com/vcm/vintage_clothing_map/VintageClothingMapApplication.java
@@ -0,0 +1,13 @@
+package com.vcm.vintage_clothing_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintageClothingMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintageClothingMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcm/vintage_clothing_map/VintageClothingMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcm.vintage_clothing_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintageClothingMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
